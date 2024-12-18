178611796a820b52a4709b7d5801b806c09a15b9
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-18 19:19:25 +0800

    sync cf (#4)
    * 重构TTS工厂，移除XTTS类，更新音频数据处理，优化缓存机制，修改WebSocket URL和引用ID
    
    * 更新启动脚本，添加证书文件和密钥文件参数，修改端口号为20000
    
    * 更新启动脚本，修改端口号，调整描述信息，新增启动脚本start_app_vc_gpu2.sh
    
    * Update package-lock.json
    
    * 更新WebSocket URL，修改引用ID字段名称为reference_id
    
    * 更新Next.js配置，移除HTTPS设置，修改构建脚本以支持导出

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-const fs = require('fs');
-const path = require('path');
-
-module.exports = {
-  devServer: {
-    https: {
-      key: fs.readFileSync(path.join(__dirname, 'cf.key')),
-      cert: fs.readFileSync(path.join(__dirname, 'cf.pem1')),
-    },
-  },
+const nextConfig = {
+  output: 'export',
 };
+
+module.exports = nextConfig;
