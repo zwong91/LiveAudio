3341cfc612c5e9590e843f409a1a94f06f0d5b6f
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-18 13:46:19 +0800

    更新WebSocket地址，修改nginx配置以使用HTTP协议进行代理

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -110,7 +110,7 @@ export default function Home() {
     }
   }
 
-  const SOCKET_URL = "wss://gtp.aleopool.cc/stream";
+  const SOCKET_URL = "wss://audio.enty.services/stream-vc";
   let manualDisconnect = false; // 标志位
 
   // Initialize WebSocket and media devices
