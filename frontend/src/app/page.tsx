450ae4c3fd6068a1131dfcc82e8d740a564a490c
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-23 19:01:09 +0800

    stream

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -197,7 +197,7 @@ export default function Home() {
                               speed: 1.0,            // Speech speed
                               volume: 0              // Volume adjustment in dB
                             },
-                            reference_id: "c9cf4e49"   // A unique reference ID
+                            vc_uid: "c9cf4e49"   // A unique reference ID
                           }
                         };
                         // Encode the data using MessagePack
