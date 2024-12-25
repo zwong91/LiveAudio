07ac315163ccdd9c54a79a183c48c14a8bbcdb84
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-25 13:56:35 +0800

    reset interrupt flag

--- src/buffering_strategy/buffering_strategies.py
+++ src/buffering_strategy/buffering_strategies.py
@@ -87,7 +87,6 @@ class SilenceAtEndOfChunk(BufferingStrategyInterface):
             self.client.scratch_buffer += self.client.buffer
             self.client.buffer.clear()
             self.processing_flag = True
-            self.interrupt_flag = False
             # schedule the processing in a separate task
             asyncio.create_task(
                 self.process_audio_async(websocket, vad_pipeline, asr_pipeline, llm_pipeline, tts_pipeline)
@@ -150,3 +149,4 @@ class SilenceAtEndOfChunk(BufferingStrategyInterface):
                 self.client.increment_file_counter()
 
         self.processing_flag = False
+        self.interrupt_flag = False
