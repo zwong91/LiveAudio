fa124e2d8552212db683fccd143563ff459d0705
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-25 17:40:46 +0800

    stream_chunk_size=20,

--- src/tts/xtts.py
+++ src/tts/xtts.py
@@ -146,7 +146,7 @@ class XTTS_v2(TTSInterface):
             gpt_cond_latent,
             speaker_embedding,
             # Streaming
-            stream_chunk_size=256,
+            stream_chunk_size=20,
             overlap_wav_len=1024,
             # GPT inference
             temperature=0.01,
