f547f60f791ac78bc816f78e663207089f020d96
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-25 18:53:57 +0800

    pyannote_args

--- src/buffering_strategy/buffering_strategies.py
+++ src/buffering_strategy/buffering_strategies.py
@@ -140,6 +140,11 @@ class SilenceAtEndOfChunk(BufferingStrategyInterface):
                                 await websocket.send_bytes(chunk)
                             else:
                                 raise StopAsyncIteration
+                        end = time.time()
+                        print(f"total processing time: {end - start}, text: {tts_text}")
+                        self.client.history = updated_history
+                        self.client.scratch_buffer.clear()
+                        self.client.increment_file_counter()
 
                 except StopAsyncIteration:
                     self.interrupt_flag = False
@@ -150,12 +155,6 @@ class SilenceAtEndOfChunk(BufferingStrategyInterface):
                     
                 except Exception as e:
                     print(f"An error occurred during TTS: {e}") 
-
-                end = time.time()
-                print(f"total processing time: {end - start}, text: {tts_text}")
-                self.client.history = updated_history
-                self.client.scratch_buffer.clear()
-                self.client.increment_file_counter()
 
         self.processing_flag = False
         self.interrupt_flag = False
