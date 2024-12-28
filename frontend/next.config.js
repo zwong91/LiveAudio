df002336bd02ab770442694c682a4797125e1e1f
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-28 22:52:47 +0800

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // 设置输出为静态导出
+  experimental: {
+    appDir: true, // 启用 App Router
+  },
 };
 
 module.exports = nextConfig;
