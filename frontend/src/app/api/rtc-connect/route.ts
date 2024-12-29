07be60da98f3a5ced0ec928f4dfb9fe8f2ba4258
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-29 17:56:26 +0800

    result: any

--- frontend/src/app/api/rtc-connect/route.ts
+++ frontend/src/app/api/rtc-connect/route.ts
@@ -11,7 +11,7 @@ const cors = Cors({
 
 function runMiddleware(req: NextRequest, res: NextResponse, fn: Function) {
   return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
+    fn(req, res, (result: any) => {
       if (result instanceof Error) {
         return reject(result);
       }
