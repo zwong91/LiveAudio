c9b9c3d10740221df7e04bec016401730d77bda3
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-29 20:53:50 +0800

    dist/*.wasm

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -21,15 +21,15 @@ const nextConfig = {
         patterns: [
           {
             from: "node_modules/onnxruntime-web/dist/*.wasm",
-            to: "../public/[name][ext]",
+            to: "public/[name][ext]",
           },
           {
             from: "node_modules/@ricky0123/vad-web/dist/vad.worklet.bundle.min.js",
-            to: "../public/[name][ext]",
+            to: "public/[name][ext]",
           },
           {
             from: "node_modules/@ricky0123/vad-web/dist/*.onnx",
-            to: "../public/[name][ext]",
+            to: "public/[name][ext]",
           },
         ],
       })
