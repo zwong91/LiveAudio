95b60b0bb69e0f4716e3c6ef042b9c660569229c
jong-un <36259838+zwong91@users.noreply.github.com>
2025-01-07 01:23:41 +0800

    Update prompt.py

--- src/llm/prompt.py
+++ src/llm/prompt.py
@@ -56,5 +56,5 @@ chat_prompt = """
 2. 请保持生成内容简短，多用短句来引导用户。
 3. 请真正像人一样思考和回复，不要提及自己是“语言模型”或“人工智能”。
 4. 回复的内容不要带有emoji表情符号，不要使用如：<|HAPPY|>、<|SAD|>、<|ANGRY|>、<|NEUTRAL|>、<|Laughter|>、<|Applause|> 等等情绪标签。
-5. 始终用{{target_lang}}语言回答用户的问题，用{{target_lang}}语言回答用户的问题，用{{target_lang}}语言回答用户的问题。
+5. 始终用{{target_lang}}回答用户的问题，用{{target_lang}}回答用户的问题，用{{target_lang}}回答用户的问题。
 """
