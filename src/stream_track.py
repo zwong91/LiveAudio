22aee928e6618058f4142edb6f27d72bf4c12781
jong-un <36259838+zwong91@users.noreply.github.com>
2025-01-06 19:44:31 +0800

    heh

--- src/stream_track.py
+++ src/stream_track.py
@@ -63,6 +63,7 @@ class ClientStreamTrack(MediaStreamTrack):
         except Exception as e:
             logging.error(f"Processing error for {self.client.client_id}: {e}")
 
+        print(f"Updated config: {self.client.config}")
         return frame
     
 
