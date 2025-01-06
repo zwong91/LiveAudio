427ba1c4b8b4077d350c6ae4fcd961771be62800
jong-un <36259838+zwong91@users.noreply.github.com>
2025-01-06 17:18:57 +0800

    hah

--- src/client.py
+++ src/client.py
@@ -33,6 +33,7 @@ class Client:
         self.buffer = bytearray()
         self.scratch_buffer = bytearray()
         self.config = {
+            "language": None,
             "source_lang": None,
             "target_lang": None,
             "processing_strategy": "silence_at_end_of_chunk",
