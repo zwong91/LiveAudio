2de8e28ea475fdf0b30106dfafb09a3a225995e8
jong-un <36259838+zwong91@users.noreply.github.com>
2025-01-07 14:28:07 +0800

    silero vad

--- src/vad/silero_vad.py
+++ src/vad/silero_vad.py
@@ -5,10 +5,11 @@ import numpy as np
 
 class SileroVAD(VADInterface):
     def __init__(self,  **kwargs):
+        # 加载 Silero VAD 模型
         self.model, utils = torch.hub.load(
             repo_or_dir="snakers4/silero-vad", model="silero_vad", force_reload=False, onnx=False,
         )
-
+        # 获取需要的工具方法
         (
             self.get_speech_timestamps,
             self.save_audio,
@@ -17,25 +18,30 @@ class SileroVAD(VADInterface):
             self.collect_chunks,
         ) = utils
 
-        self.sampling_rate = sampling_rate = 16000
-        self.collar = 1.0
+        self.sampling_rate = sampling_rate = 16000 # 采样率
 
     async def detect_activity(self, client):
+        """
+        使用Silero VAD模型进行语音活动检测。
+        :param client: 传入的客户端对象，应该包含音频数据
+        :return: 语音段落的时间戳列表
+        """
         frames = np.frombuffer(client.scratch_buffer, dtype=np.int16)
-
         # normalization see https://discuss.pytorch.org/t/torchaudio-load-normalization-question/71470
         frames = frames / (1 << 15)
-
-        audio = torch.tensor(frames.astype(np.float32))
+        audio_tensor = torch.tensor(frames.astype(np.float32))
+        # 获取语音时间戳
         vad_results = self.get_speech_timestamps(
-            audio, self.model, sampling_rate=self.sampling_rate, threshold = 0.5,
-            min_speech_duration_ms = 250, max_speech_duration_s = float('inf'), min_silence_duration_ms = 500, speech_pad_ms = 30
+            audio_tensor,
+            self.model,
+            sampling_rate=self.sampling_rate,
+            threshold=0.5,  # 可以根据需要调整阈值
+            min_speech_duration_ms=250,
+            max_speech_duration_s=float('inf'),
+            min_silence_duration_ms=500,
+            speech_pad_ms=30
         )
   
-        vad_segments = []
-        if len(vad_results) > 0:
-            vad_segments = [
-                {"start": segment["start"] / 1000 , "end": segment["end"] / 1000, "confidence": 1.0}
-                for segment in vad_results
-            ]
+        # 返回语音时间段（以秒为单位）
+        vad_segments = [{"start": segment["start"] / 1000, "end": segment["end"] / 1000, "confidence": 1.0} for segment in vad_results]
         return vad_segments
