1f7a920392cac5c01fd45d3b2a4b58716484eaf7
jong-un <36259838+zwong91@users.noreply.github.com>
2025-01-08 16:02:04 +0800

    AudioSegment

--- src/tts/gtts.py
+++ src/tts/gtts.py
@@ -10,6 +10,8 @@ from .tts_interface import TTSInterface
 from src.utils.audio_utils import wave_header_chunk
 import langid
 
+from pydub import AudioSegment
+
 from gtts import gTTS
 import gtts.lang
 
