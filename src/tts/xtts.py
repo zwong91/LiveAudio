8258872f7eb317d8d76d5b23ff548f0c168b677d
jong-un <36259838+zwong91@users.noreply.github.com>
2025-01-10 19:55:30 +0800

    chunk length

--- src/tts/xtts.py
+++ src/tts/xtts.py
@@ -168,7 +168,7 @@ class XTTS_v2(TTSInterface):
             gpt_cond_latent,
             speaker_embedding,
             # Streaming
-            stream_chunk_size=256,
+            stream_chunk_size=512,
             overlap_wav_len=1024,
             # GPT inference
             temperature=0.01,
@@ -240,7 +240,7 @@ class XTTS_v2(TTSInterface):
             gpt_cond_latent,
             speaker_embedding,
             # Streaming reduce it to get faster response, but degrade quality
-            stream_chunk_size=20,
+            stream_chunk_size=40,
             overlap_wav_len=1024,
             # GPT inference
             temperature=0.01,
