4b288108e83865a3e8611d875864c4903c80b336
jong-un <36259838+zwong91@users.noreply.github.com>
2025-01-10 18:10:07 +0800

    gtts stream

--- src/tts/gtts.py
+++ src/tts/gtts.py
@@ -94,11 +94,3 @@ class GTTS(TTSInterface):
                 pcm_data_16K = audio_resampled.raw_data
                 # 使用 wave_header_chunk 发送处理后的数据
                 yield wave_header_chunk(pcm_data_16K, 1, 2, 16000)
-
-        #send silent audio
-        if not simultaneous:
-            audio = AudioSegment.from_wav(self.silence_wav)
-            # 重采样为 16kHz，单声道，16-bit
-            audio_resampled = audio.set_frame_rate(16000).set_channels(1).set_sample_width(2)
-            pcm_data_16K = audio_resampled.raw_data
-            yield wave_header_chunk(pcm_data_16K, 1, 2, 16000)
