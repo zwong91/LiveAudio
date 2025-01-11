a484cd44b4f6b288daefa559006394975507babc
Chunk <155896034+jong-un-crypto@users.noreply.github.com>
2025-01-11 09:19:05 +0000

    tauri

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,12 +10,6 @@ const nextConfig = {
 
   // Transpile the specific packages you need
   transpilePackages: ['onnxruntime-web', '@ricky0123/vad-web'],
-
-  // 注意：在 SSG 模式下使用 Next.js 的 Image 组件需要此功能。
-  // 请参阅 https://nextjs.org/docs/messages/export-image-api 了解不同的解决方法。
-  images: {
-      unoptimized: true,
-  },
   // 配置 assetPrefix，否则服务器无法正确解析您的资产。
   assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
 
