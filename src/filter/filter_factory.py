588f420e7a7b6b53ef8b380ed5a73bd34126e891
jong-un <36259838+zwong91@users.noreply.github.com>
2025-05-16 14:43:15 +0800

    ➕ Add `DeepNetFilter` class in `deepnet_filter.py` and update `FilterFactory` to support `deepfilter` type

--- src/filter/filter_factory.py
+++ src/filter/filter_factory.py
@@ -1,9 +1,12 @@
 from .noisereduce_filter import NoisereduceFilter
+from .deepnet_filter import DeepNetFilter
 
 class FilterFactory:
     @staticmethod
     def create_filter_pipeline(filter_type, **kwargs):
         if filter_type == "noisereduce":
             return NoisereduceFilter(**kwargs)
+        elif filter_type == "deepfilter":
+            return DeepNetFilter(**kwargs)
         else:
             raise ValueError(f"Unknown Filter pipeline type: {filter_type}")
