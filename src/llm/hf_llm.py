3d7ab65c853c0385bb63f7d88b97817eaefef400
jong-un-crypto <155896034+jong-un-crypto@users.noreply.github.com>
2025-05-19 02:40:38 +0800

    gaunernst/gemma-3-27b-it-int4-awq

--- src/llm/hf_llm.py
+++ src/llm/hf_llm.py
@@ -16,7 +16,7 @@ Always answer as helpfully as possible, while being safe. Your answers should no
 If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
 
 class HFLLM(LLMInterface):
-    MODEL_ID = "google/gemma-3-27b-it"
+    MODEL_ID = "gaunernst/gemma-3-27b-it-int4-awq"
 
     def __init__(self):
         engine_args = AsyncEngineArgs(
