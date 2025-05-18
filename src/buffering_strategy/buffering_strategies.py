416615c43fe471cfc6a58cbc60884b3f9aebed9c
jong-un-crypto <155896034+jong-un-crypto@users.noreply.github.com>
2025-05-19 04:09:47 +0800

    last_speaking_time

--- src/buffering_strategy/buffering_strategies.py
+++ src/buffering_strategy/buffering_strategies.py
@@ -137,7 +137,7 @@ class SilenceAtEndOfChunk(BufferingStrategyInterface):
             #FIXME: 这里最大timeout是 3s, 不能无限等待如果一直未检测到完整对话
             endpointing_delay = self._max_endpointing_delay
 
-        extra_sleep = last_speaking_time + endpointing_delay - time.time()
+        extra_sleep = self.last_speaking_time + endpointing_delay - time.time()
         timeout = max(extra_sleep, 0)
         if timeout > 0:
             #await asyncio.sleep(timeout)
