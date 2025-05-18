1daf2f32f32cea8d92c6d7982d2c27f913cceea2
jong-un-crypto <155896034+jong-un-crypto@users.noreply.github.com>
2025-05-19 03:29:32 +0800

    Ngrok tunnel

--- src/server.py
+++ src/server.py
@@ -727,7 +727,7 @@ class Server:
     async def start_server(self):
         """Start the Uvicorn server as a coroutine."""
         # Open Ngrok tunnel
-        listener = ngrok.forward(f"http://localhost:{self.port}")
+        listener = await ngrok.forward(f"http://localhost:{self.port}")
         print(f"Ngrok tunnel opened at {listener.url()} for port {self.port}")
         NGROK_URL = listener.url()
         INCOMING_CALL_ROUTE = "/twilio/inbound_call"
