41fa153ad4d88f1adf108b1d29617c11c9061ea1
jong-un <36259838+zwong91@users.noreply.github.com>
2025-05-20 12:31:27 +0800

    af_nova

--- src/filter/deepnet_filter.py
+++ src/filter/deepnet_filter.py
@@ -14,7 +14,7 @@ except ModuleNotFoundError as e:
 class DeepNetFilter(FilterInterface):
     def __init__(self, **kwargs):
         self._sample_rate = 16000
-        self._model = kwargs.get("model", "DeepFilterNet2")
+        self._model = kwargs.get("df", "DeepFilterNet2")
         self._filtering = True
 
         try:
--- src/tts/kokoro.py
+++ src/tts/kokoro.py
@@ -38,7 +38,7 @@ class Kokoro(TTSInterface):
     ALL_VOICES = [v for voices in VOICES.values() for v in voices]
 
 
-    def __init__(self, voice: str = "af_alloy(2)+af_nova(1)"):
+    def __init__(self, voice: str = "af_nova"):
         self.VOICE = voice
         print(f"Using voice: {self.VOICE}")
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
