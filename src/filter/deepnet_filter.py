8b246065a6a9fe22a5295aa95a5f6b878be9c86b
jong-un <36259838+zwong91@users.noreply.github.com>
2025-05-22 15:24:10 +0800

    DeepNetFilter

--- src/filter/deepnet_filter.py
+++ src/filter/deepnet_filter.py
@@ -1,6 +1,6 @@
 import numpy as np
-import logging
 import torch
+import logging
 from .filter_interface import FilterInterface
 
 logger = logging.getLogger(__name__)
@@ -11,16 +11,14 @@ except ModuleNotFoundError as e:
     logger.error(f"Missing deepfilter: {e}")
     raise Exception("Please install deepfilterlib: pip install deepfilternet")
 
+
 class DeepNetFilter(FilterInterface):
     def __init__(self, **kwargs):
-        self._sample_rate = 16000
-        self._model = kwargs.get("model", "DeepFilterNet2")
         self._filtering = True
 
         try:
             # Initialize model
-            self.df_state = init_df()
-            logger.info(f"Loaded {self._model}")
+            self.model, self.df_state, _ = init_df()
         except Exception as e:
             logger.error(f"Failed to load DeepNetFilter: {e}")
             self._filtering = False
@@ -34,16 +32,18 @@ class DeepNetFilter(FilterInterface):
             data = np.frombuffer(audio, dtype=np.int16)
             audio_float = data.astype(np.float32) / 32768.0
 
+            audio_tensor = torch.from_numpy(audio_float).float()
+            audio_tensor = audio_tensor.unsqueeze(0)
             # Process audio
             enhanced = enhance(
-                audio_float,
+                self.model,
                 self.df_state,
-                sr=self._sample_rate,
-                atten_lim_db=6,
+                audio_tensor,
+                atten_lim_db=6, #噪声衰减限制（attenuation limit）
             )
 
             # Convert back to int16
-            processed = (enhanced * 32768.0).astype(np.int16)
+            processed = (enhanced.squeeze(0).numpy() * 32768.0).astype(np.int16)
             return processed.tobytes()
 
         except Exception as e:
