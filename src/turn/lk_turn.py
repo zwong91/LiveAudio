d140e96d6e589e090d6ec9c2a18891bff44ce311
jong-un <36259838+zwong91@users.noreply.github.com>
2025-05-24 02:30:14 +0800

    Update lk_turn.py

--- src/turn/lk_turn.py
+++ src/turn/lk_turn.py
@@ -39,14 +39,14 @@ class LKTurn(TurnInterface):
                 filename=ONNX_FILENAME,
                 subfolder="onnx",
                 revision=MODEL_REVISION,
-                local_files_only=False,# 本地没有就下载
+                local_files_only=True,
             )
 
             config_fname = hf_hub_download(
                 repo_id=HG_MODEL,
                 filename="languages.json",
                 revision=MODEL_REVISION,
-                local_files_only=False,# 本地没有就下载
+                local_files_only=True,
             )
             with open(config_fname) as f:
                 self.languages = json.load(f)
@@ -59,7 +59,7 @@ class LKTurn(TurnInterface):
             self.tokenizer = AutoTokenizer.from_pretrained(
                 HG_MODEL,
                 revision=MODEL_REVISION,
-                local_files_only=False,# 本地没有就下载
+                local_files_only=True,# 本地没有就下载
                 truncation_side="left",
             )
             self._last_language = None
