b27e3d2aafcdedd9cf6c4074756aeca32d07661b
jong-un <36259838+zwong91@users.noreply.github.com>
2025-05-24 04:37:42 +0800

    languages.json

--- download_files.py
+++ download_files.py
@@ -1,9 +1,18 @@
 from huggingface_hub import hf_hub_download
 
-path = hf_hub_download(
+local_path = hf_hub_download(
     repo_id="livekit/turn-detector",
     filename="onnx/model_q8.onnx",
     revision="v0.2.0-intl"
 )
 
-print("File downloaded to:", path)
+print("File downloaded to:", local_path)
+
+
+conf_path = hf_hub_download(
+    repo_id="livekit/turn-detector",
+    filename="languages.json",
+    revision="v0.2.0-intl"
+)
+
+print("Json File downloaded to:", conf_path)
